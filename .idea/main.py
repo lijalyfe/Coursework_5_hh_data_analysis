db7ef1405edf196f13b2f2572a43507e73da39eb
lija_lyfe <lija6556@list.ru>
2023-08-15 01:23:31 +0500

    call function get_all_vacancies

--- .idea/main.py
+++ .idea/main.py
@@ -6,3 +6,8 @@ db_manager = DBManager()
 companies_and_vacancies_count = db_manager.get_companies_and_vacancies_count()
 for company in companies_and_vacancies_count:
     print(company[0], company[1])
+
+# получаем список всех вакансий
+all_vacancies = db_manager.get_all_vacancies()
+for vacancy in all_vacancies:
+    print(vacancy[0], vacancy[1], vacancy[2], vacancy[3], vacancy[4])
