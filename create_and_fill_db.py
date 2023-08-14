eb0528b787f9013ccfe3f079f4daec52e3b14058
lija_lyfe <lija6556@list.ru>
2023-08-15 00:07:49 +0500

    connection to the database

--- create_and_fill_db.py
+++ create_and_fill_db.py
@@ -2,3 +2,6 @@ import requests
 import psycopg2
 from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
+# Подключение к БД
+conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+cursor = conn.cursor()
