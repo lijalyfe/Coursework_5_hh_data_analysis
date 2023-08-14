9a833e8114c211d23d8e93a868b41039d693b138
lija_lyfe <lija6556@list.ru>
2023-08-15 00:26:26 +0500

    getting a list of companies and the number of vacancies for each

--- .idea/db_manager.py
+++ .idea/db_manager.py
@@ -7,3 +7,9 @@ class DBManager:
         self.conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
         self.cursor = self.conn.cursor()
 
+    def get_companies_and_vacancies_count(self):
+        """получает список всех компаний и количество вакансий у каждой компании"""
+        self.cursor.execute(
+            "SELECT companies.name, COUNT(vacancies.id) AS vacancies_count FROM companies LEFT JOIN vacancies ON companies.id=vacancies.employer_id GROUP BY companies.id ORDER BY vacancies_count DESC"
+        )
+        return self.cursor.fetchall()
