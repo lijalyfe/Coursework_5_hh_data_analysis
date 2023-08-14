8af3e64ef3ebe8716e12c5ce9880fcad56dba16a
lija_lyfe <lija6556@list.ru>
2023-08-15 00:08:47 +0500

    creating tables

--- create_and_fill_db.py
+++ create_and_fill_db.py
@@ -5,3 +5,23 @@ from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 # Подключение к БД
 conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASSWORD)
 cursor = conn.cursor()
+
+# Создание таблиц
+cursor.execute("""
+CREATE TABLE companies (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    url TEXT
+);
+""")
+cursor.execute("""
+CREATE TABLE vacancies (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    salary_min INTEGER,
+    salary_max INTEGER,
+    employer_id INTEGER,
+    FOREIGN KEY (employer_id) REFERENCES companies (id)
+);
+""")
+conn.commit()
