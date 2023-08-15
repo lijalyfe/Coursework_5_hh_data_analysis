146872c299f9a3c4513bfc4864cadf429cf7c89d
lija_lyfe <lija6556@list.ru>
2023-08-15 19:28:36 +0500

    update create_and_fill_db.py

--- create_and_fill_db.py
+++ create_and_fill_db.py
@@ -15,7 +15,7 @@ if 'companies' not in existing_tables:
     cursor.execute("""
     CREATE TABLE companies (
         id SERIAL PRIMARY KEY,
-        name TEXT,
+        name TEXT UNIQUE,
         url TEXT
     );
     """)
@@ -27,6 +27,7 @@ if 'vacancies' not in existing_tables:
         name TEXT,
         salary_min INTEGER,
         salary_max INTEGER,
+        url TEXT,
         employer_id INTEGER,
         FOREIGN KEY (employer_id) REFERENCES companies (id)
     );
@@ -37,35 +38,34 @@ conn.commit()
 # Получение данных с API
 companies_url = 'https://api.hh.ru/employers'
 response = requests.get(companies_url)
-companies_data = response.json()['items']
+companies_data = response.json()['items'][:10]
 
 vacancies_url = 'https://api.hh.ru/vacancies'
-vacancies_data = []
+n_vacancies_per_company = 10
 
 for company in companies_data:
+    cursor.execute("SELECT id FROM companies WHERE name=%s", (company["name"],))
+    company_id_exists = cursor.fetchone()
+
+    if not company_id_exists:
+        cursor.execute(
+            "INSERT INTO companies (name, url) VALUES (%s, %s) RETURNING id", (company["name"], company["url"])
+        )
+        company_id = cursor.fetchone()[0]
+    else:
+        company_id = company_id_exists[0]
+
     company_vacancies_url = f'{vacancies_url}/?employer_id={company["id"]}'
     response = requests.get(company_vacancies_url)
-    company_vacancies_data = response.json()['items']
-    vacancies_data.extend(company_vacancies_data)
+    vacancies_data = response.json()['items']
 
-# Заполнение таблиц данными
-for company in companies_data:
-    cursor.execute("INSERT INTO companies (name, url) VALUES (%s, %s)", (company["name"], company["url"]))
-
-conn.commit()  # Добавление всех компаний в БД перед добавлением вакансий
-
-for vacancy in vacancies_data:
-    employer_id = vacancy["employer"]["id"]
-
-    cursor.execute("SELECT id FROM companies WHERE id=%s", (employer_id,))
-
-    if cursor.fetchone() is not None:
+    for vacancy in vacancies_data[:n_vacancies_per_company]:
         salary = vacancy.get('salary')
         salary_min = salary.get('from') if salary else None
         salary_max = salary.get('to') if salary else None
         cursor.execute(
-            "INSERT INTO vacancies (name, salary_min, salary_max, employer_id) VALUES (%s, %s, %s, %s)",
-            (vacancy["name"], salary_min, salary_max, employer_id)
+            "INSERT INTO vacancies (name, salary_min, salary_max, url, employer_id) VALUES (%s, %s, %s, %s, %s)",
+            (vacancy["name"], salary_min, salary_max, vacancy["url"], company_id)
         )
 
 conn.commit()
